21e53d0b2a4b480d4347854d2ef48d6b63a1ad9b
Igor Zinar <igorzinar@gmail.com>
2021-01-11 15:42:14 +0200

    install labrary uuid, create base of songs, import to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 import './styles/app.scss'
 import Song from './components/Song'
 import Player from './components/Player'
+import data from './components/Utilits'
+
 
 function App() {
+
+//   const [songs, setSongs] = useState(data())
+// const [currentSong, setCurrentSong] = useState(songs.map(song => `<div>song</div>`))
+// console.log(currentSong)
+
   return (
     <div className="App">
       <Song/>
       <Player/>
+      
     </div>
   );
 }
