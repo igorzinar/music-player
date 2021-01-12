eea048f89f05dd614130319e397c2a9b9d86b888
Igor Zinar <igorzinar@gmail.com>
2021-01-12 14:55:16 +0200

    make library components to show playlist and style it

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import './styles/app.scss'
 import Song from './components/Song'
 import Player from './components/Player'
 import data from './components/Utilits'
+import Library from './components/Library'
 
 
 function App() {
 
   const [songs, setSongs] = useState(data())
-const [currentSong, setCurrentSong] = useState(songs.[4])
-console.log(currentSong)
+const [currentSong, setCurrentSong] = useState(songs[0])
+const [isPlaying, setIsPlaying] = useState(false)
 
   return (
     <div className="App">
-      <Song currentSong={currentSong} />
-      <Player/>
-      
+      <Song currentSong={currentSong}  />
+      <Player currentSong={currentSong} setIsPlaying={setIsPlaying} isPlaying={isPlaying} />
+      <Library songs={songs}/>
     </div>
   );
 }
